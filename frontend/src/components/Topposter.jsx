5044f0a93d361e0b320b01b54af101caa6a93175
AkashdeepSingh <142108012+akashdeepsingh283@users.noreply.github.com>
2026-05-18 00:08:25 +0530

    Update Topposter.jsx

--- frontend/src/components/Topposter.jsx
+++ frontend/src/components/Topposter.jsx
@@ -3,7 +3,7 @@ import img1 from '../img/bg.png';
 
 const Topposter = () => {
   return (
-    <div className="w-full h-screen overflow-hidden">
+    <div className="mt-12 w-full h-screen overflow-hidden">
       <img
         src={img1}
         alt=""
